b64ea92af09fb270b2e94f84e905d668feb1f32a
silentvik <vikentsi.zaitsau@gmail.com>
2021-12-29 18:21:52 +0300

    Initial commit. Some docs added.

--- the_equilator/exceptions.py
+++ the_equilator/exceptions.py
@@ -8,11 +8,10 @@ def inspect_func(depth=1):
         caller_frame = caller_frame.f_back
     code_obj = caller_frame.f_code
     code_obj_name = code_obj.co_name
-    print("Имя вызывающего объекта: ", code_obj_name)
+    print(f"Имя вызывающего объекта: {code_obj_name}")
 
 
 class EquilatorError(Exception):
     """
         Raised when can't equilate
     """
-    pass
